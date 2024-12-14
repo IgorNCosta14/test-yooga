50d8b72ac218ae3e18366e878dcd8371f19d7b74
Igor Nascimento Costa <igorncosta14@gmail.com>
2024-12-14 20:34:07 -0300

    unit tests

---
+++ src/tests/NearbyUseCase.test.ts
@@ -0,0 +1,133 @@
+import { IGoogleMapsAdapter } from "../interfaces/googleMapsAdapter.interface";
+import { NearbyUseCase } from "../useCase/Nearby.useCase";
+
+describe("NearbyUseCase", () => {
+    let nearbyUseCase: NearbyUseCase;
+    let mockGoogleMapsAdapter: jest.Mocked<IGoogleMapsAdapter>;
+
+    beforeEach(() => {
+        mockGoogleMapsAdapter = {
+            fetchNearbyPlaces: jest.fn()
+        };
+
+        nearbyUseCase = new NearbyUseCase(mockGoogleMapsAdapter);
+    });
+
+    it("should return nearby places for each coordinate", async () => {
+        mockGoogleMapsAdapter.fetchNearbyPlaces.mockResolvedValue({
+            results: [
+                {
+                    name: "Place 1",
+                    vicinity: "Address 1",
+                    geometry: {
+                        location: {
+                            lat: 1,
+                            lng: 1
+                        }
+                    }
+                }, {
+                    name: "Place 2",
+                    vicinity: "Address 2",
+                    geometry: {
+                        location: {
+                            lat: 2,
+                            lng: 2
+                        }
+                    }
+                }, {
+                    name: "Place 3",
+                    vicinity: "Address 3",
+                    geometry: {
+                        location: {
+                            lat: 3,
+                            lng: 3
+                        }
+                    }
+                }, {
+                    name: "Place 4",
+                    vicinity: "Address 4",
+                    geometry: {
+                        location: {
+                            lat: 4,
+                            lng: 4
+                        }
+                    }
+                }, {
+                    name: "Place 5",
+                    vicinity: "Address 5",
+                    geometry: {
+                        location: {
+                            lat: 5,
+                            lng: 5
+                        }
+                    }
+                }
+            ]
+        });
+
+        const coordinates = [
+            { latitude: -22.951321545009705, longitude: -43.21040413125191 },
+            { latitude: 30.328676601368205, longitude: 35.44443730801659 },
+        ];
+
+        const result = await nearbyUseCase.execute({ coordinates });
+
+        expect(result).toEqual([
+            {
+                point: 1,
+                coordinates: { latitude: -22.951321545009705, longitude: -43.21040413125191 },
+                nearbyPlaces: [
+                    { name: "Place 1", address: "Address 1", location: { lat: 1, lng: 1 } },
+                    { name: "Place 2", address: "Address 2", location: { lat: 2, lng: 2 } },
+                    { name: "Place 3", address: "Address 3", location: { lat: 3, lng: 3 } },
+                    { name: "Place 4", address: "Address 4", location: { lat: 4, lng: 4 } },
+                    { name: "Place 5", address: "Address 5", location: { lat: 5, lng: 5 } }
+                ]
+            },
+            {
+                point: 2,
+                coordinates: { latitude: 30.328676601368205, longitude: 35.44443730801659 },
+                nearbyPlaces: [
+                    { name: "Place 1", address: "Address 1", location: { lat: 1, lng: 1 } },
+                    { name: "Place 2", address: "Address 2", location: { lat: 2, lng: 2 } },
+                    { name: "Place 3", address: "Address 3", location: { lat: 3, lng: 3 } },
+                    { name: "Place 4", address: "Address 4", location: { lat: 4, lng: 4 } },
+                    { name: "Place 5", address: "Address 5", location: { lat: 5, lng: 5 } }
+                ]
+            }
+        ]);
+
+        expect(mockGoogleMapsAdapter.fetchNearbyPlaces).toHaveBeenCalledTimes(2);
+        
+        expect(mockGoogleMapsAdapter.fetchNearbyPlaces).toHaveBeenCalledWith({ 
+            lat: -22.951321545009705, 
+            lon: -43.21040413125191 
+        });
+
+        expect(mockGoogleMapsAdapter.fetchNearbyPlaces).toHaveBeenCalledWith({ 
+            lat: 30.328676601368205, 
+            lon: 35.44443730801659 
+        });
+    });
+
+    it("should throw an error if Google Maps Adapter fails", async () => {
+        mockGoogleMapsAdapter.fetchNearbyPlaces.mockRejectedValue(
+            new Error("Error when searching: Google Maps API error")
+        );
+
+        const coordinates = [
+            { latitude: 10, longitude: 20 }
+        ];
+
+        await expect(
+            nearbyUseCase.execute({ coordinates })
+        ).rejects.toThrow("Error when searching: Google Maps API error");
+
+        expect(mockGoogleMapsAdapter.fetchNearbyPlaces).toHaveBeenCalledTimes(1);
+
+        expect(mockGoogleMapsAdapter.fetchNearbyPlaces).toHaveBeenCalledWith({ 
+            lat: 10, 
+            lon: 20 
+        });
+    });
+})
---
+++ src/tests/Utils.test.ts
@@ -0,0 +1,110 @@
+import { Utils } from '../utils';
+import fs from 'fs';
+
+jest.mock('fs');
+
+describe('Utils - Test haversine method', () => {
+    const utils = new Utils();
+
+    it('should calculate the distance between two pairs of coordinates', () => {
+        const distance = utils.haversine({
+            lat1: 0,
+            lon1: 0,
+            lat2: 0,
+            lon2: 1
+        });
+
+        expect(distance).toBeCloseTo(111195, 0);
+    });
+
+    it('should return an error if the latitude is out of range', () => {
+        expect(() => {
+            utils.haversine({
+                lat1: 100,
+                lon1: 0,
+                lat2: 0,
+                lon2: 1
+            });
+        }).toThrow('Invalid coordinates. Ensure latitudes are between -90 and 90, and longitudes are between -180 and 180.');
+    });
+
+    it('should return an error if the longitude is out of range', () => {
+        expect(() => {
+            utils.haversine({
+                lat1: 0,
+                lon1: 200,
+                lat2: 0,
+                lon2: 1
+            });
+        }).toThrow('Invalid coordinates. Ensure latitudes are between -90 and 90, and longitudes are between -180 and 180.');
+    });
+});
+
+describe('Utils - Test validateCoordinatesFile method', () => {
+    const utils = new Utils();
+
+    it('should return true if the coordinates file is valid', () => {
+        const mockFileContent = `Latitude Longitude\n-22.951321545009705, -43.21040413125191\n40.43214447302087, 116.57037490892017`;
+
+        (fs.readFileSync as jest.Mock).mockReturnValue(mockFileContent);
+
+        const result = utils.validateCoordinatesFile({ fileName: 'validFile.txt' });
+
+        expect(result).toBe(true);
+    });
+
+    it('should return false if the coordinates file is invalid', () => {
+        const mockFileContent = `Latitude Longitude\naaaaa, bbbbb`;
+
+        (fs.readFileSync as jest.Mock).mockReturnValue(mockFileContent);
+
+        const result = utils.validateCoordinatesFile({ fileName: 'invalidFile.txt' });
+
+        expect(result).toBe(false);
+    });
+
+    it('should return false if the coordinates file does not exist', () => {
+        (fs.readFileSync as jest.Mock).mockImplementation(() => {
+            throw new Error('File not found');
+        });
+
+        const result = utils.validateCoordinatesFile({ fileName: 'nonExistentFile.txt' });
+
+        expect(result).toBe(false);
+    });
+})
+
+describe('Utils - Test readCoordinatesFile method', () => {
+    const utils = new Utils();
+
+    it('should correctly parse a valid coordinates file', () => {
+        const mockFileContent = `Latitude Longitude\n-90, -180\n90, 180`;
+        (fs.readFileSync as jest.Mock).mockReturnValue(mockFileContent);
+
+        const result = utils.readCoordinatesFile({ fileName: 'validFile.txt' });
+
+        expect(result).toEqual([
+            { latitude: -90, longitude: -180 },
+            { latitude: 90, longitude: 180 }
+        ]);
+    });
+
+    it('should return an empty array if the file is empty', () => {
+        const mockFileContent = `Latitude Longitude`;
+        (fs.readFileSync as jest.Mock).mockReturnValue(mockFileContent);
+
+        const result = utils.readCoordinatesFile({ fileName: 'emptyFile.txt' });
+
+        expect(result).toEqual([]);
+    });
+
+    it('should return an error if the parse fails', () => {
+        (fs.readFileSync as jest.Mock).mockImplementation(() => {
+            throw new Error('File not found');
+        });
+
+        expect(() => {
+            utils.readCoordinatesFile({ fileName: 'nonExistentFile.txt' });
+        }).toThrow("Failed to read coordinates file.");
+    });
+});
